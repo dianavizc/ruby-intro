5cdd0aa0252f8b95c977b195829b377620f97ff0
Diana Vizcarra <diana.vizcarra@kellogg.northwestern.edu>
2025-01-28 20:09:06 +0000

    Data changes

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,51 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 2
+puts 5
+
+puts 5 + 2
+puts 5 * 2
+puts 5 - 2
+puts 5 / 2.0
+
+puts (2 + 5) * 5
 
 # Non-numbers
+puts "Hello, world!"
+
+puts "Tacos are" + " delish"
+
+puts "Tacos " * 3
+
+puts "Tacos" + 3.to_s # "to make 3 a string"
 
 # True, False
 
+puts true
+puts false
+
 # Nothing
+puts nil
 
 # Variables
+food = "tacos"
+quantity = 3
+
+puts food * quantity
 
 # Combine strings and variables
+puts "tacos: #{quantity}"
+
+puts "#{food}: #{quantity}" # This is how you tell ruby that you want to add a variable to the string in {}
 
 # String manipulation
+puts "Hello".length
+puts "how are you today".capitalize # Capital letter to the first letter
+puts "computer".reverse
+
+creed = "This Is The Way"
+
+puts creed.upcase
+puts creed.downcase
+puts creed.swapcase
